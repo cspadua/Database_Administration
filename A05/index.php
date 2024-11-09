54e7c7a5f2f0e4c37a1695173e97097598e31f4b
cspadua <carlpadua1217@gmail.com>
2024-11-09 13:06:29 +0800

    added icon

--- A05/index.php
+++ A05/index.php
@@ -38,6 +38,7 @@ $result = executeQuery($query);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="images/icon.png">
 </head>
 
 <body data-bs-theme="dark" id="body">
